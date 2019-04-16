8c1b6d51436a322c8c57d50571dc1db5f2db9297
amzachrich14 <43038415+amzachrich14@users.noreply.github.com>
2019-04-16 11:35:16 -0400

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -24,37 +24,64 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+    delete timer;
 }
 
 void MainWindow::paintEvent(QPaintEvent *e)
 {
     QPainter painter(this);
 
+    if(gameStart==1){
          painter.drawPixmap(0,0,800,600,*background);
 
          painter.drawPixmap(player_one->playerX,player_one->playerY,50,50,*(player_one->getSprite()));
-
+         update();
+}
 
 }
 
 void MainWindow::keyPressEvent(QKeyEvent *evt)
 {
-
-    if ((evt->key()==Qt::Key_Left || evt->key()==Qt::Key_A) && direction != 4 ) {
+    if(gameStart==1){
+    if ((evt->key()==Qt::Key_Left || evt->key()==Qt::Key_A)) {
         direction=1;
         qDebug()<< "left";
 
-    } else if ((evt->key()==Qt::Key_Right || evt->key()==Qt::Key_D) && direction != 3 ) {
+    } else if ((evt->key()==Qt::Key_Right || evt->key()==Qt::Key_D)) {
         direction=2;
         qDebug()<< "right";
-           }
+           } else if ((evt->key()==Qt::Key_Space)){
+        qDebug()<<"Shoot";
+    }
 
+}
 }
 void MainWindow::updateCoordinate()
 {
 
 
-    if(direction==1) player_one->playerX-= 10;
-    if(direction==2) player_one->playerX+= 10;
+    if(direction==1 && player_one->playerX>0){ player_one->playerX-= 10; direction=0;}
+    if(direction==2 && player_one->playerX<750){ player_one->playerX+= 10; direction=0;}
+
 
     }
+
+
+void MainWindow::on_titleStart_clicked()
+{
+    gameStart = 1;
+    ui->stackedWidget->setCurrentIndex(2);
+    timer->start(50);
+}
+
+void MainWindow::on_titleControls_clicked()
+{
+     ui->stackedWidget->setCurrentIndex(1);
+}
+
+void MainWindow::on_controlsStart_clicked()
+{
+    gameStart = 1;
+    ui->stackedWidget->setCurrentIndex(2);
+    timer->start(50);
+}
--- mainwindow.h
+++ mainwindow.h
@@ -26,19 +26,25 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
     QTimer *timer;
+    bool gameStart=0;
 
 private:
     Ui::MainWindow *ui;
     QPixmap* background;
     player* player_one;
-    int direction;
+    int direction =0;
 
 protected:
 void paintEvent(QPaintEvent *e);
 void keyPressEvent(QKeyEvent *evt);
+
+
+
+private slots:
+void on_titleStart_clicked();
 void updateCoordinate();
-
-
+void on_titleControls_clicked();
+void on_controlsStart_clicked();
 };
 
 #endif // MAINWINDOW_H
